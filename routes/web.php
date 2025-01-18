05ad1703d17760e9765f1c659f05e4d966d18a0d
rzrusu <rzrusu03@gmail.com>
2025-01-18 14:50:38 +0200

    fix: add get routes for normally post only to redirect refreshes

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,19 @@ use App\Http\Controllers\SpotifyController;
 
 Route::get('/', [SpotifyController::class, 'index']);
 Route::post('/search', [SpotifyController::class, 'search']);
+Route::post('/search-preview', [SpotifyController::class, 'searchPreview']);
+Route::post('/add-albums', [SpotifyController::class, 'addAlbums']);
+
+// add get routes for search results that just redirect to / incase of a refresh
+Route::get('/search-results', function () {
+    return redirect('/');
+});
+
+Route::get('/add-albums', function () {
+    return redirect('/');
+});
+
+
+Route::get('/search-preview', function () {
+    return redirect('/');
+});
