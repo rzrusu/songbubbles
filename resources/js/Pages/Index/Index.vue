71c2101fa3df5bd527c44b5ee620816182902c94
rzrusu <rzrusu03@gmail.com>
2025-01-23 18:04:03 +0200

    feat: remove play buttons from  track cards

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -536,7 +536,7 @@ onUnmounted(() => {
           ></div>
           <div
             class="w-full h-full py-4 flex flex-col items-center"
-            :style="{ height: track.size / 1.2 + 'px' }"
+            :style="{ height: track.size / 3 + 'px' }"
           >
             <div class="flex flex-col items-center h-full justify-center">
               <div
@@ -551,41 +551,6 @@ onUnmounted(() => {
               >
                 {{ track.artists.map(artist => artist.name).join(', ') }}
               </div>
-            </div>
-            <div class="flex flex-row items-center space-x-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                :width="track.size / 7 + 'px'"
-                :height="track.size / 7 + 'px'"
-              >
-                <path
-                  d="M9.195 18.44c1.25.714 2.805-.189 2.805-1.629v-2.34l6.945 3.968c1.25.715 2.805-.188 2.805-1.628V8.69c0-1.44-1.555-2.343-2.805-1.628L12 11.029v-2.34c0-1.44-1.555-2.343-2.805-1.628l-7.108 4.061c-1.26.72-1.26 2.536 0 3.256l7.108 4.061Z"
-                />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                :width="track.size / 7 + 'px'"
-                :height="track.size / 7 + 'px'"
-              >
-                <path
-                  d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z"
-                />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                :width="track.size / 7 + 'px'"
-                :height="track.size / 7 + 'px'"
-              >
-                <path
-                  d="M5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v8.122c0 1.44 1.555 2.343 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256l-7.108-4.061C13.555 6.346 12 7.249 12 8.689v2.34L5.055 7.061Z"
-                />
-              </svg>
             </div>
           </div>
         </div>
